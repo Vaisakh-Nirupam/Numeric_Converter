42c5a2b049f8cfc84ec5c7e7d514590e1ebcf53c
Vaisakh-Nirupam <vaisakhnirupamvn@gmail.com>
2025-06-13 22:12:38 +0530

    Adding CSS
    - Created CSS file and style.css
    - Imported the css file into app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# Importing Libraries
 import streamlit as st
 import pandas as pd
 
+# Full page
+st.set_page_config(page_title="LPA", page_icon="Images/LPA Logo.png", layout="wide")
+
+# Adding styles
+with open("css/Style.css") as css:
+    st.markdown(f"<style>{css.read()}</style>", unsafe_allow_html=True)
